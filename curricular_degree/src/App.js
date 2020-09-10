42791c7b3fa95f4cca925d454d27f98498200660
Lucas Soares <lucassoares81@hotmail.com>
2020-09-10 18:17:49 -0300

    Adicionando a configuração do firebase

--- curricular_degree/src/App.js
+++ curricular_degree/src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./firebase";
+
+function createMateria() {
+  const data = {
+    name: "Programação",
+    nota: 7,
+  };
+  firebase.database().ref("materias/").set(data);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={(e) => createMateria()}>ENVIAR</button>
       </header>
     </div>
   );
