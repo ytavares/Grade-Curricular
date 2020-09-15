b093356ddc3488f5093e7fab74768b30f3964b9d
Lucas da Silva Soares <lucassoares81@hotmail.com>
2020-09-15 01:48:07 +0000

    Tela de login , layout criado

--- curricular_degree/src/Pages/Login/index.js
+++ curricular_degree/src/Pages/Login/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
-
+import './styles.css'
+import { GiNinjaHead } from "react-icons/gi";
 
 export default function Login() {
-    return (<div><h1>Login</h1></div>)
+    return (
+
+        <div className="content">
+            <div className="main-login">
+                <GiNinjaHead size={100} />
+
+                <form >
+                    <div className="form-inputs">
+                        <input
+                            type="text"
+
+                            placeholder="E-mail"
+
+                        ></input>
+                        <input
+                            type="password"
+
+                            placeholder="Senha"
+
+                        ></input>
+                    </div>
+                    <button>ACESSAR</button>
+                </form>
+            </div>
+        </div>
+
+    )
 }
